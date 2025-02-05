6112eced998ff05ee11a31de861ee00852ab3c04
Lucas Safeno <lugnorio@gmail.com>
2025-02-05 16:02:51 -0300

    Carrinho de Compras com PHP#02 - Listando os produtos na página inicial

---
+++ App/Helpers/products.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+
+    1 => ["name" => "Product 1", "price" => 3400],
+    2 => ["name" => "Product 2", "price" => 100],
+    3 => ["name" => "Product 3", "price" => 2300],
+    4 => ["name" => "Product 4", "price" => 5000],
+    5 => ["name" => "Product 5", "price" => 3600],
+    6 => ["name" => "Product 6", "price" => 4900],
+
+];
+
+?>
